f559c98d0717ea2abf57582eb51e846662a45c2a
Peter Meckiffe <peterf.meckiffe@gmail.com>
2015-09-09 14:32:11 +0100

    last few unit tests & code working

--- PhonebookTestCase.py
+++ PhonebookTestCase.py
@@ -18,7 +18,9 @@ class PhonebookTestCase(unittest.TestCase):
         os.unlink(phonebook.app.config['DATABASE'])
 
     def test_empty_db(self):
-        data = json.loads(self.app.get('/').data.decode('utf-8'))
+        get_request = self.app.get('/list')
+        data = json.loads(get_request.data.decode('utf-8'))
+        assert get_request.status_code == 200
         assert [] == data
 
     def test_search_entry(self):
@@ -28,29 +30,35 @@ class PhonebookTestCase(unittest.TestCase):
             telephone='07872124086',
             address='3 Wren Close, Winch',
         ), follow_redirects=True)
-        assert post_result.data.decode('utf-8') == ""
+        assert post_result.data.decode('utf-8') == "Success"
+        assert post_result.status_code == 200
         post_result = self.app.post('/add', data=dict(
             forename='Peter',
             surname='Norris',
             telephone='07872124086',
             address='3 Wren Close, Winch',
         ), follow_redirects=True)
-        assert post_result.data.decode('utf-8') == ""
+        assert post_result.data.decode('utf-8') == "Success"
+        assert post_result.status_code == 200
         post_result = self.app.post('/add', data=dict(
             forename='John',
             surname='Meckiffe',
             telephone='07972124086',
             address='3 Wren Close, Winch',
         ), follow_redirects=True)
-        assert post_result.data.decode('utf-8') == ""
+        assert post_result.data.decode('utf-8') == "Success"
+        assert post_result.status_code == 200
         post_result = self.app.post('/add', data=dict(
             forename='Peter',
             surname='Meckiffe',
             telephone='07872124086',
             address='3 Wren Close, Winch',
         ), follow_redirects=True)
-        assert post_result.data.decode('utf-8') == ""
-        data = self.app.get('/search/Meckiffe').data.decode('utf-8')
+        assert post_result.data.decode('utf-8') == "Success"
+        assert post_result.status_code == 200
+        get_request = self.app.get('/search/Meckiffe')
+        data = get_request.data.decode('utf-8')
+        assert get_request.status_code == 200
         assert [{'id':4,'forename': 'Peter', 'surname': 'Meckiffe', \
                  'telephone': "07872124086", 'address': '3 Wren Close, Winch'}, \
                 {'id':3,'forename': 'John', 'surname': 'Meckiffe', \
@@ -63,28 +71,31 @@ class PhonebookTestCase(unittest.TestCase):
             telephone='07872124086',
             address='3 Wren Close, Winch',
         ), follow_redirects=True)
-        assert post_result.data.decode('utf-8') == ""
+        assert post_result.data.decode('utf-8') == "Success"
+        assert post_result.status_code == 200
         post_result = self.app.post('/add', data=dict(
             forename='Peter',
             surname='Norris',
             telephone='07872124086',
             address='3 Wren Close, Winch',
         ), follow_redirects=True)
-        assert post_result.data.decode('utf-8') == ""
+        assert post_result.data.decode('utf-8') == "Success"
+        assert post_result.status_code == 200
         post_result = self.app.post('/add', data=dict(
             forename='John',
             surname='Meckiffe',
             telephone='07972124086',
             address='3 Wren Close, Winch',
         ), follow_redirects=True)
-        assert post_result.data.decode('utf-8') == ""
+        assert post_result.data.decode('utf-8') == "Success"
         post_result = self.app.post('/add', data=dict(
             forename='Peter',
             surname='Meckiffe',
             telephone='07872124086',
             address='3 Wren Close, Winch',
         ), follow_redirects=True)
-        assert post_result.data.decode('utf-8') == ""
+        assert post_result.data.decode('utf-8') == "Success"
+        assert post_result.status_code == 200
         data = self.app.get('/search/meckiffe').data.decode('utf-8')
         assert [{'id':4,'forename': 'Peter', 'surname': 'Meckiffe', \
                  'telephone': "07872124086", 'address': '3 Wren Close, Winch'}, \
@@ -103,12 +114,102 @@ class PhonebookTestCase(unittest.TestCase):
             telephone='07872124086',
             address='3 Wren Close, Winch',
         ), follow_redirects=True)
-        assert post_result.data.decode('utf-8') == ""
-        confirmation = json.loads(self.app.get('/').data.decode('utf-8'))[0]
+        assert post_result.data.decode('utf-8') == "Success"
+        assert post_result.status_code == 200
+        confirmation = json.loads(self.app.get('/list').data.decode('utf-8'))[0]
         assert confirmation['forename'] == 'Peter'
         assert confirmation['surname'] == 'Meckiffe'
         assert confirmation['telephone'] == '07872124086'
         assert confirmation['address'] == '3 Wren Close, Winch'
+
+    def test_update_entry(self):
+        post_result = self.app.post('/add', data=dict(
+            forename='Peter',
+            surname='Meckiffe',
+            telephone='07872124086',
+            address='3 Wren Close, Winch',
+        ), follow_redirects=True)
+        assert post_result.data.decode('utf-8') == "Success"
+        assert post_result.status_code == 200
+        update_result = self.app.put('/update', data=dict(
+            id=1,
+            forename='Petert',
+            surname='Meckiffet',
+            telephone='07872124085',
+            address='3 Wren Close, Wincht',
+        ), follow_redirects=True)
+        assert update_result.data.decode('utf-8') == "Success"
+        assert update_result.status_code == 200
+        confirmation = json.loads(self.app.get('/list').data.decode('utf-8'))
+        assert len(confirmation) == 1
+
+        assert confirmation[0]['forename'] == 'Petert'
+        assert confirmation[0]['surname'] == 'Meckiffet'
+        assert confirmation[0]['telephone'] == '07872124085'
+        assert confirmation[0]['address'] == '3 Wren Close, Wincht'
+
+        update_result = self.app.put('/update', data=dict(
+            id=1,
+            forename='Peterz',
+        ), follow_redirects=True)
+        assert update_result.data.decode('utf-8') == "Success"
+        assert update_result.status_code == 200
+        confirmation = json.loads(self.app.get('/list').data.decode('utf-8'))
+        assert len(confirmation) == 1
+
+        assert confirmation[0]['forename'] == 'Peterz'
+        assert confirmation[0]['surname'] == 'Meckiffet'
+        assert confirmation[0]['telephone'] == '07872124085'
+        assert confirmation[0]['address'] == '3 Wren Close, Wincht'
+
+        update_result = self.app.put('/update', data=dict(
+            id=1,
+            forename='Petery',
+            surname='Meckiffey',
+        ), follow_redirects=True)
+        print(update_result.data.decode('utf-8'))
+        assert update_result.data.decode('utf-8') == "Success"
+        assert update_result.status_code == 200
+        confirmation = json.loads(self.app.get('/list').data.decode('utf-8'))
+        assert len(confirmation) == 1
+
+        assert confirmation[0]['forename'] == 'Petery'
+        assert confirmation[0]['surname'] == 'Meckiffey'
+        assert confirmation[0]['telephone'] == '07872124085'
+        assert confirmation[0]['address'] == '3 Wren Close, Wincht'
+
+        update_result = self.app.put('/update', data=dict(
+            id=1,
+            forename='Peterl',
+            surname='Meckiffel',
+            telephone='07872124089',
+        ), follow_redirects=True)
+        assert update_result.data.decode('utf-8') == "Success"
+        assert update_result.status_code == 200
+        confirmation = json.loads(self.app.get('/list').data.decode('utf-8'))
+        assert len(confirmation) == 1
+
+        assert confirmation[0]['forename'] == 'Peterl'
+        assert confirmation[0]['surname'] == 'Meckiffel'
+        assert confirmation[0]['telephone'] == '07872124089'
+        assert confirmation[0]['address'] == '3 Wren Close, Wincht'
+
+        update_result = self.app.put('/update', data=dict(
+            forename='Peterl',
+            surname='Meckiffel',
+            telephone='07872124089',
+        ), follow_redirects=True)
+        assert update_result.data.decode('utf-8') == "Please provide a valid ID"
+        assert update_result.status_code == 400
+
+        update_result = self.app.put('/update', data=dict(
+            id=1,
+            forename='Peterl',
+            surname='Meckiffel',
+            telephone='0787',
+        ), follow_redirects=True)
+        assert update_result.data.decode('utf-8') == "Please enter valid phone number"
+        assert update_result.status_code == 400
 
     def test_db_increment(self):
         post_result = self.app.post('/add', data=dict(
@@ -117,8 +218,9 @@ class PhonebookTestCase(unittest.TestCase):
             telephone='07872124086',
             address='3 Wren Close, Winch',
         ), follow_redirects=True)
-        assert post_result.data.decode('utf-8') == ""
-        confirmation = json.loads(self.app.get('/').data.decode('utf-8'))
+        assert post_result.data.decode('utf-8') == "Success"
+        assert post_result.status_code == 200
+        confirmation = json.loads(self.app.get('/list').data.decode('utf-8'))
         assert len(confirmation) == 1
         assert confirmation[0]['id'] == 1
         post_result = self.app.post('/add', data=dict(
@@ -127,8 +229,9 @@ class PhonebookTestCase(unittest.TestCase):
             telephone='07872124086',
             address='3 Wren Close, Winch',
         ), follow_redirects=True)
-        assert post_result.data.decode('utf-8') == ""
-        confirmation = json.loads(self.app.get('/').data.decode('utf-8'))
+        assert post_result.data.decode('utf-8') == "Success"
+        assert post_result.status_code == 200
+        confirmation = json.loads(self.app.get('/list').data.decode('utf-8'))
         assert len(confirmation) == 2
         assert (confirmation[0]['id'] == 2 and confirmation[1]['id'] == 1) or (
         confirmation[1]['id'] == 2 and confirmation[0]['id'] == 1)
@@ -139,14 +242,15 @@ class PhonebookTestCase(unittest.TestCase):
             surname='Meckiffe',
             telephone='07872124086',
         ), follow_redirects=True)
-        assert post_result.data.decode('utf-8') == ""
+        assert post_result.data.decode('utf-8') == "Success"
+        assert post_result.status_code == 200
         post_result = self.app.post('/add', data=dict(
             forename='Peter',
             surname='Meckiffe',
             telephone='07872124086',
         ), follow_redirects=True)
-        assert post_result.data.decode('utf-8') == ""
-        assert len(json.loads(self.app.get('/').data.decode('utf-8'))) == 1
+        assert post_result.status_code == 401
+        assert len(json.loads(self.app.get('/list').data.decode('utf-8'))) == 1
 
     def test_optional_address(self):
         post_result = self.app.post('/add', data=dict(
@@ -154,12 +258,53 @@ class PhonebookTestCase(unittest.TestCase):
             surname='Meckiffe',
             telephone='07872124086',
         ), follow_redirects=True)
-        assert post_result.data.decode('utf-8') == ""
-        confirmation = json.loads(self.app.get('/').data.decode('utf-8'))[0]
+        assert post_result.data.decode('utf-8') == "Success"
+        assert post_result.status_code == 200
+        confirmation = json.loads(self.app.get('/list').data.decode('utf-8'))[0]
         assert confirmation['forename'] == 'Peter'
         assert confirmation['surname'] == 'Meckiffe'
         assert confirmation['telephone'] == '07872124086'
         assert confirmation['address'] == ''
+
+    def test_delete_id(self):
+        post_result = self.app.post('/add', data=dict(
+            forename='Peter',
+            surname='Meckiffe',
+            telephone='07872124086',
+            address='3 Wren Close, Winch',
+        ), follow_redirects=True)
+        assert post_result.data.decode('utf-8') == "Success"
+        assert post_result.status_code == 200
+        post_result = self.app.post('/add', data=dict(
+            forename='Rob',
+            surname='Mack',
+            telephone='12345678987',
+            address='3 Oldham St',
+        ), follow_redirects=True)
+        assert post_result.data.decode('utf-8') == "Success"
+        assert post_result.status_code == 200
+        delete_assert = self.app.delete('/delete/1')
+        assert delete_assert.data.decode('utf-8') == "Success"
+        assert delete_assert.status_code == 200
+        confirmation = json.loads(self.app.get('/list').data.decode('utf-8'))
+        assert len(confirmation) == 1
+        assert confirmation[0]['forename'] == 'Rob'
+        assert confirmation[0]['surname'] == 'Mack'
+        assert confirmation[0]['telephone'] == '12345678987'
+        assert confirmation[0]['address'] == '3 Oldham St'
+
+    def test_invalid_number(self):
+        post_result = self.app.post('/add', data=dict(
+            forename='Peter',
+            surname='Meckiffe',
+            telephone='j123',
+            address='3 Wren Close, Winch',
+        ), follow_redirects=True)
+        assert post_result.data.decode('utf-8') == "Please enter valid phone number"
+        assert post_result.status_code == 400
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
--- phonebook.py
+++ phonebook.py
@@ -3,7 +3,7 @@ __author__ = 'petermeckiffe'
 import sqlite3
 from contextlib import closing
 from flask import Flask, g, jsonify, request, flash, make_response
-import json
+import json, re
 
 # configuration
 DATABASE = '/tmp/phonebook.db'
@@ -34,8 +34,8 @@ def teardown_request(exception):
     if db is not None:
         db.close()
 
-@app.route('/', methods=['GET'])
-def show_entries():
+@app.route('/list', methods=['GET'])
+def list_entries():
     cur = g.db.execute('select * from phonebook order by id desc')
     entries = [dict(id=row[0], forename=row[1], surname=row[2], telephone=row[3], address=row[4]) for row in cur.fetchall()]
     return make_response(json.dumps(entries), 200)
@@ -46,16 +46,68 @@ def add_entry():
         addr = request.form['address']
     else:
         addr = ""
+    if not valid_phone_number(request.form['telephone']):
+            return make_response("Please enter valid phone number", 400)
+
+    if is_dup(request.form['forename'], request.form['surname'], request.form['telephone'], addr):
+        return make_response("", 401)
+
     g.db.execute('insert into phonebook (forename, surname, telephone, address) values (?, ?, ?, ?)',
                  [request.form['forename'], request.form['surname'], request.form['telephone'], addr])
     g.db.commit()
-    return make_response("", 200)
+    return make_response("Success", 200)
+
+def is_dup(forname, surname,tel, addr):
+    cur = g.db.execute('select * from phonebook WHERE forename = "{}" \
+    AND surname = "{}" \
+    AND telephone = "{}" \
+    AND address = "{}" order by id desc'.format(forname, surname,tel, addr))
+    if len(list(cur.fetchall())) > 0:
+        return True
+    else:
+        return False
+
+@app.route('/update', methods=['PUT'])
+def update_entry():
+    if 'id' not in request.form:
+        return make_response("", 400)
+    else:
+        cur = g.db.execute('select * from phonebook WHERE id = "{}"'.format(request.form['id']))
+        if len(list(cur.fetchall())) == 0:
+            return make_response("", 404)
+    if 'address' in request.form:
+        g.db.execute('update phonebook set address = "{}" where id = {}'.format(request.form['address'],request.form['id']))
+    if 'forename' in request.form:
+        g.db.execute('update phonebook set forename = "{}" where id = {}'.format(request.form['forename'],request.form['id']))
+    if 'surname' in request.form:
+        g.db.execute('update phonebook set surname = "{}" where id = {}'.format(request.form['surname'],request.form['id']))
+    if 'telephone' in request.form:
+        if not valid_phone_number(request.form['telephone']):
+            return make_response("Please enter valid phone number", 400)
+        g.db.execute('update phonebook set telephone = "{}" where id = {}'.format(request.form['telephone'],request.form['id']))
+
+    g.db.commit()
+    return make_response("Success", 200)
+
+def valid_phone_number(telephone):
+    p = re.compile('(^[+0-9]{1,3})*([0-9]{10,11}$)')
+    if p.match(request.form['telephone']) is None:
+        return False
+    else:
+        return True
+
 
 @app.route('/search/<name>')
 def search_entries(name):
     cur = g.db.execute('select * from phonebook WHERE surname LIKE "%{}%" order by id desc'.format(name))
     entries = [dict(id=row[0], forename=row[1], surname=row[2], telephone=row[3], address=row[4]) for row in cur.fetchall()]
     return make_response(json.dumps(entries), 200)
+
+@app.route('/delete/<id>', methods=['DELETE'])
+def delete_entry(id):
+    g.db.execute('delete from phonebook where id = {}'.format(id))
+    g.db.commit()
+    return make_response("", 200)
 
 if __name__ == '__main__':
     app.run()
